b45921ff7a00113bb905d436daea1d21f281fd01
took13 <took13@hotmail.com>
2016-12-23 16:59:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => '[my reply]'
+				'text' => '[this is automatically reply]' //$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
