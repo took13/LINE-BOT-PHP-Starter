987257d7159678bfd91f556426e11b6d87995a58
took13 <took13@hotmail.com>
2017-05-24 15:06:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'this is a automatically reply from PINotification LINE@' //$text
+				'text' => 'Hi, this is official LINE for PINotification from Asset Optimization.' //$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
