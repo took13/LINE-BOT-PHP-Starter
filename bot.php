e62fb40184feb1a8de6aa8e2f247c3e9df2cfac6
took13 <took13@hotmail.com>
2018-06-26 18:44:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 			
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$displayname],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
